ea28f5cd1b0d41b56ea679192dd6965fe192b865
HaywardMorihara <nathanielmorihara@gmail.com>
2021-11-07 10:30:17 -0500

    Basic tiles, camera, house, player w/ movement & collisions

---
+++ game/KinematicBody2D.gd
@@ -0,0 +1,30 @@
+extends KinematicBody2D
+
+
+var speed = 80
+var velocity = Vector2.ZERO
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass	
+
+func _get_input():
+	velocity = Vector2.ZERO
+	if Input.is_action_pressed('ui_right'):
+		velocity.x += 1
+	if Input.is_action_pressed('ui_left'):
+		velocity.x -= 1
+	if Input.is_action_pressed('ui_down'):
+		velocity.y += 1
+	if Input.is_action_pressed('ui_up'):
+		velocity.y -= 1
+	velocity = velocity.normalized() * speed
+	
+func _physics_process(delta):
+	_get_input()
+	velocity = move_and_slide(velocity)
