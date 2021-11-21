c39f4e667e466bafd2a4455fe0b45f57237dae34
HaywardMorihara <nathanielmorihara@gmail.com>
2021-11-21 10:06:37 -0500

    Basic (non-functional) Menu Flow

---
+++ menus/buttons/LoadVillageMenuButton.gd
@@ -0,0 +1,5 @@
+extends Button
+
+
+func _on_LoadVillageMenuButton_pressed():
+	get_tree().change_scene("res://menus/LoadVillageMenu.tscn")
---
+++ menus/buttons/NewVillageButton.gd
@@ -0,0 +1,5 @@
+extends Button
+
+
+func _on_NewVillageButton_pressed():
+	get_tree().change_scene("res://menus/NewVillageMenu.tscn")
