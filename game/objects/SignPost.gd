d68dfa3d2871eaf325801e0e99590017f4862679
HaywardMorihara <nathanielmorihara@gmail.com>
2021-12-07 08:32:58 -0500

    Tweak sign post popup

--- game/objects/SignPost.gd
+++ game/objects/SignPost.gd
@@ -7,8 +7,9 @@ func _ready():
 func _on_Area2D_body_entered(body):
 	if $ObjectPlacement.is_placed and body.name == "Player":
 		# Needed, because not using popup(), because that seems to reset the position
+		$SignPostPopup.popup()
 		$SignPostPopup.enable()
-		$SignPostPopup.rect_position = self.position + Vector2(-47.0, -50.0)
+		$SignPostPopup.rect_position = self.position + Vector2(-47.0, -25.0)
 		$SignPostPopup.show()
 
 
